79576cf44db918482f16712737ae998faae7fdde
Raina To The Moon <mds6425@gmail.com>
2025-03-06 00:00:32 +0900

    Delete backend directory

--- backend/src/routes/goalRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { createGoalHandler, updateGoalHandler } from '../controllers/goalController';
-
-const router = Router();
-
-router.post('/goals', createGoalHandler);
-router.put('/goals/:id', updateGoalHandler);
-
-export default router;
--- backend/src/routes/postRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { createPostHandler, getAllPostsHandler } from '../controllers/postController';
-
-const router = Router();
-
-router.post('/posts', createPostHandler);
-router.get('/posts', getAllPostsHandler);
-
-export default router;
