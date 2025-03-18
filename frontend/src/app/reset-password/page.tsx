eeb5a187a54681b13bd8afb573ff223d0db69e7d
Raina Moon <mds6425@gmail.com>
2025-03-19 02:17:39 +0900

    ✨ Feature: Create reset password page component to render ResetPasswordForm

---
+++ frontend/src/app/reset-password/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import React from 'react'
+import ResetPasswordForm from './ResetPasswordForm';
+
+const page = () => {
+  return (
+    <ResetPasswordForm />
+  )
+}
+
+export default page
