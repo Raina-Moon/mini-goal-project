fa86ba254e19fa79a5dbf8b0cd3aa317f385048d
Raina Moon <mds6425@gmail.com>
2025-03-29 15:27:35 +0900

    ♻️ refactor: Simplify goal creation logic in page component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,25 +1,18 @@
 "use client";
 
-import { useEffect } from "react";
 import GoalForm from "../components/GoalForm";
 import { useAuth } from "./contexts/AuthContext";
-import { useGoals } from "./contexts/GoalContext";
 
 const page = () => {
   const { user } = useAuth();
-  const { goals, fetchGoals } = useGoals();
-
-  useEffect(() => {
-    if (user?.id) {
-      fetchGoals(user.id).catch((err) =>
-        console.error("Failed to load goals", err)
-      );
-    }
-  }, [user?.id, fetchGoals]);
 
   return (
     <>
-      <GoalForm onGoalCreated={() => user?.id && fetchGoals(user.id)} />
+      <GoalForm
+        onGoalCreated={() => {
+          console.log("Goal created");
+        }}
+      />
     </>
   );
 };
