b2c13727e0375c66f7dedb8cf1040c3f5a339ea4
Raina Moon <mds6425@gmail.com>
2025-03-30 00:56:50 +0900

    ✨ feat: Enhance FollowersList to navigate to user dashboard on click

--- frontend/src/app/dashboard/[userId]/components/FollowersList.tsx
+++ frontend/src/app/dashboard/[userId]/components/FollowersList.tsx
@@ -1,14 +1,27 @@
-import { Follower } from "@/utils/api";
+"use client";
+
+import { Follower, User } from "@/utils/api";
+import { fetchApi } from "@/utils/api/fetch";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 interface FollowersListProps {
   followers: Follower[];
 }
 
-const FollowersList = ({ followers }: FollowersListProps) =>
+const FollowersList = ({ followers }: FollowersListProps) => {
+  const router = useRouter();
+  const [viewedUser, setViewedUser] = useState<User | null>(null);
+
+  const goToDashboard = (userId: number) => {
+    router.push(`/dashboard/${userId}`);
+  }
+
+  return (
   followers.length > 0 && (
     <ul className="mb-6 space-y-2">
       {followers.map((follower) => (
-        <li key={follower.id} className="flex items-center gap-3">
+        <li onClick={() => goToDashboard(follower.id)} key={follower.id} className="flex items-center gap-3">
           <img
             src={follower.profile_image ?? "/images/DefaultProfile.png"}
             alt={follower.username}
@@ -18,6 +31,8 @@ const FollowersList = ({ followers }: FollowersListProps) =>
         </li>
       ))}
     </ul>
+  )
   );
+};
 
 export default FollowersList;
