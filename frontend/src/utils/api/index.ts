a3c76c558c3c0b2cc464fce1378f24c1407cf36d
Raina Moon <mds6425@gmail.com>
2025-03-30 17:17:21 +0900

    🔥chore : Remove unused API utility files for auth, comments, followers, goals, likes, localStorage, and posts

--- frontend/src/utils/api/index.ts
+++ frontend/src/utils/api/index.ts
@@ -1,7 +1,1 @@
 export * from "./types";
-export * from "./auth";
-export * from "./goals";
-export * from "./followers";
-export * from "./posts";
-export * from "./comments";
-export * from "./likes";
