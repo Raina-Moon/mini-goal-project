04aee6d1b20a4854ac50a2eee5812f70f3d5067a
Raina Moon <mds6425@gmail.com>
2025-04-01 18:30:59 +0900

    ✨ feat: Add service worker for handling push notifications and notification clicks

---
+++ frontend/public/sw.js
@@ -0,0 +1,37 @@
+self.addEventListener("push", (event) => {
+  const data = event.data.json();
+  const { title, body } = data;
+
+  const options = {
+    body: body,
+    icon: "./images/TimerLogo.png",
+    vibrate: [200, 100, 200],
+    data: { url: "/profile" },
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(title, options).then(() => {
+      self.clients.matchAll().then((clients) => {
+        clients.forEach((client) =>
+          client.postMessage({ type: "PLAY_SOUND", url: "./sounds/notify.wav" })
+        );
+      });
+    })
+  );
+});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: "window" }).then((clientList) => {
+      for (const client of clientList) {
+        if (client.url === event.notification.data.url && "focus" in client) {
+          return client.focus();
+        }
+      }
+      if (clients.openWindow) {
+        return clients.openWindow(event.notification.data.url);
+      }
+    })
+  );
+});
