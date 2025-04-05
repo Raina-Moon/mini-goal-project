b93749da3879c3ed7b543923242eb390972ef15a
Raina Moon <mds6425@gmail.com>
2025-04-05 23:58:53 +0900

    💄 ui: Update PostHeader component to include userId in button navigation

--- frontend/src/components/PostsList/PostHeader.tsx
+++ frontend/src/components/PostsList/PostHeader.tsx
@@ -1,19 +1,28 @@
+import { useRouter } from "next/navigation";
+
 interface PostHeaderProps {
-    username: string;
-    profileImage?: string;
-  }
-  
-  const PostHeader = ({ username, profileImage }: PostHeaderProps) => (
+  userId: number | null;
+  username: string;
+  profileImage?: string;
+}
+
+const PostHeader = ({ userId, username, profileImage }: PostHeaderProps) => {
+  const router = useRouter();
+
+  return (
     <div className="flex flex-row items-center gap-2 mb-2">
-      <img
-        src={profileImage || "/images/DefaultProfile.png"}
-        alt={`${username}'s profile`}
-        className="w-8 h-8 rounded-full object-cover"
-      />
-      <span className="text-sm font-medium text-gray-800">
-        {username || "Unknown User"}
-      </span>
+      <button onClick={() => router.push(`/dashboard/${userId}`)}>
+        <img
+          src={profileImage || "/images/DefaultProfile.png"}
+          alt={`${username}'s profile`}
+          className="w-8 h-8 rounded-full object-cover"
+        />
+        <span className="text-sm font-medium text-gray-800">
+          {username || "Unknown User"}
+        </span>
+      </button>
     </div>
   );
-  
-  export default PostHeader;
+};
+
+export default PostHeader;
