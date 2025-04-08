4337d6778f6d65e41dbb68159b5f76009e1f149a
Raina Moon <mds6425@gmail.com>
2025-04-08 21:51:20 +0900

    💚 fix: Wrap ResetPasswordForm in Suspense for improved loading state

--- frontend/src/app/reset-password/page.tsx
+++ frontend/src/app/reset-password/page.tsx
@@ -1,13 +1,21 @@
 "use client";
 
-import React from "react";
+import React, { Suspense } from "react";
 import ResetPasswordForm from "./ResetPasswordForm";
 
 const page = () => {
   return (
-    <div className="bg-primary-500 min-h-screen font-inter">
-      <ResetPasswordForm />
-    </div>
+    <Suspense
+      fallback={
+        <div className="flex justify-center items-center min-h-screen">
+          Loading...
+        </div>
+      }
+    >
+      <div className="bg-primary-500 min-h-screen font-inter">
+        <ResetPasswordForm />
+      </div>
+    </Suspense>
   );
 };
 
