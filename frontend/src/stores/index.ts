123482c303f18e0fadf01d1d5cb74ea0d0bec12e
Raina Moon <mds6425@gmail.com>
2025-04-24 04:27:33 +0900

    ✨ feat: Add bookmarksReducer to store configuration for managing bookmarks

--- frontend/src/stores/index.ts
+++ frontend/src/stores/index.ts
@@ -1,12 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit'
-import authReducer from './slices/authSlice'
-import postReducer from './slices/postSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./slices/authSlice";
+import postReducer from "./slices/postSlice";
+import bookmarksReducer from "./slices/bookmarksSlice";
 
 export const store = configureStore({
-  reducer: { auth: authReducer,
-    posts : postReducer,
-   },
-})
+  reducer: {
+    auth: authReducer,
+    posts: postReducer,
+    bookmarks: bookmarksReducer,
+  },
+});
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
