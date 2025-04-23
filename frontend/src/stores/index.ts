2b1ca8eb03e8e05999581093d374ecbd3f9f3cfb
Raina Moon <mds6425@gmail.com>
2025-04-24 04:49:06 +0900

    ✨ feat: Add comments reducer to store configuration for managing comments

--- frontend/src/stores/index.ts
+++ frontend/src/stores/index.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import postReducer from "./slices/postSlice";
 import bookmarksReducer from "./slices/bookmarksSlice";
+import commentReducer from "./slices/commentsSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     posts: postReducer,
     bookmarks: bookmarksReducer,
+    comments: commentReducer,
   },
 });
 
